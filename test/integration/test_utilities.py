6a54162a211c6c1009c35ace2c1dbd696bbdf096
jeremyr <jeremyr@themill.com>
2021-09-01 21:30:04 -0700

    Update integration tests

--- test/integration/test_utilities.py
+++ test/integration/test_utilities.py
@@ -27,8 +27,25 @@ def scene_path(temporary_directory):
     return temporary_directory
 
 
-def test_fetch_next_version(scene_path):
-    """Fetch next version from scene files."""
+def test_fetch_next_version_unmatched(scene_path):
+    """Return version 1 when no files are matching."""
+    import nomenclator.utilities
+
+    pattern = "{project}_{shot}_{description}_v{version}"
+
+    token_mapping = {
+        "project": "project3",
+        "shot": "sh001",
+        "description": "comp",
+    }
+    version = nomenclator.utilities.fetch_next_version(
+        scene_path, pattern, token_mapping
+    )
+    assert version == 1
+
+
+def test_fetch_next_version_unmatched_tokens(scene_path):
+    """Return version 1 when no files are matching with incoming tokens."""
     import nomenclator.utilities
 
     pattern = "{project}_{shot}_{description}_v{version}"
@@ -43,6 +60,13 @@ def test_fetch_next_version(scene_path):
     )
     assert version == 1
 
+
+def test_fetch_next_version_one_match(scene_path):
+    """Return version 2 when one file is matching with version 1."""
+    import nomenclator.utilities
+
+    pattern = "{project}_{shot}_{description}_v{version}"
+
     token_mapping = {
         "project": "project2",
         "shot": "sh002",
@@ -52,6 +76,13 @@ def test_fetch_next_version(scene_path):
         scene_path, pattern, token_mapping
     )
     assert version == 2
+
+
+def test_fetch_next_version_two_match(scene_path):
+    """Return version 3 when two files are matching and latest version is 2."""
+    import nomenclator.utilities
+
+    pattern = "{project}_{shot}_{description}_v{version}"
 
     token_mapping = {
         "project": "project2",
@@ -63,6 +94,13 @@ def test_fetch_next_version(scene_path):
     )
     assert version == 3
 
+
+def test_fetch_next_version_two_match_with_options(scene_path):
+    """Same scenario as previous test with username appended to latest file."""
+    import nomenclator.utilities
+
+    pattern = "{project}_{shot}_{description}_v{version}"
+
     token_mapping = {
         "project": "project2",
         "shot": "sh003",
@@ -73,12 +111,170 @@ def test_fetch_next_version(scene_path):
     )
     assert version == 3
 
-    token_mapping = {
-        "project": "project3",
-        "shot": "sh001",
-        "description": "comp",
-    }
-    version = nomenclator.utilities.fetch_next_version(
-        scene_path, pattern, token_mapping
+
+def test_fetch_template_config_matching():
+    """Return matching template configuration."""
+    import nomenclator.utilities
+    from nomenclator.config import TemplateConfig
+
+    path = "/path/my_project/ep002/sh004/scripts"
+
+    config = TemplateConfig(
+        id="Config",
+        pattern_path=r"/path/{project}/{episode:ep\d+}/{shot:sh\d+}/scripts",
+        pattern_base=r"{project}_{episode}_{shot}_v{version}.nk",
+        default_expression=r"[\w_.-]+",
+        match_start=True,
+        match_end=True,
+        outputs=None,
     )
-    assert version == 1
+
+    token_mapping = {}
+
+    result = nomenclator.utilities.fetch_template_config(
+        path, [config], token_mapping
+    )
+    assert result == config
+    assert token_mapping == {
+        "project": "my_project",
+        "episode": "ep002",
+        "shot": "sh004",
+    }
+
+
+def test_fetch_template_config_unmatched():
+    """Fail to match config when shot does not match given expression."""
+    import nomenclator.utilities
+    from nomenclator.config import TemplateConfig
+
+    path = "/path/my_project/ep002/sh004_SPECIAL/scripts"
+
+    config = TemplateConfig(
+        id="Config",
+        pattern_path=r"/path/{project}/{episode:ep\d+}/{shot:sh\d+}/scripts",
+        pattern_base=r"{project}_{episode}_{shot}_v{version}.nk",
+        default_expression=r"[\w_.-]+",
+        match_start=True,
+        match_end=True,
+        outputs=None,
+    )
+
+    token_mapping = {}
+
+    result = nomenclator.utilities.fetch_template_config(
+        path, [config], token_mapping
+    )
+    assert result is None
+    assert token_mapping == {}
+
+
+def test_fetch_template_config_unmatched_end():
+    """Fail to match config when path must match end of pattern."""
+    import nomenclator.utilities
+    from nomenclator.config import TemplateConfig
+
+    path = "/path/my_project/ep002/sh004/scripts/subfolder"
+
+    config = TemplateConfig(
+        id="Config",
+        pattern_path=r"/path/{project}/{episode:ep\d+}/{shot:sh\d+}/scripts",
+        pattern_base=r"{project}_{episode}_{shot}_v{version}.nk",
+        default_expression=r"[\w_.-]+",
+        match_start=True,
+        match_end=True,
+        outputs=None,
+    )
+
+    token_mapping = {}
+
+    result = nomenclator.utilities.fetch_template_config(
+        path, [config], token_mapping
+    )
+    assert result is None
+    assert token_mapping == {}
+
+
+def test_fetch_template_config_match_flexible_end():
+    """Same scenario as previous test but with flexible end anchor."""
+    import nomenclator.utilities
+    from nomenclator.config import TemplateConfig
+
+    path = "/path/my_project/ep002/sh004/scripts/subfolder"
+
+    config = TemplateConfig(
+        id="Config",
+        pattern_path=r"/path/{project}/{episode:ep\d+}/{shot:sh\d+}/scripts",
+        pattern_base=r"{project}_{episode}_{shot}_v{version}.nk",
+        default_expression=r"[\w_.-]+",
+        match_start=True,
+        match_end=False,
+        outputs=None,
+    )
+
+    token_mapping = {}
+
+    result = nomenclator.utilities.fetch_template_config(
+        path, [config], token_mapping
+    )
+    assert result == config
+    assert token_mapping == {
+        "project": "my_project",
+        "episode": "ep002",
+        "shot": "sh004",
+    }
+
+
+def test_fetch_template_config_unmatched_start():
+    """Fail to match config when path must match start of pattern."""
+    import nomenclator.utilities
+    from nomenclator.config import TemplateConfig
+
+    path = "/root/path/my_project/ep002/sh004/scripts"
+
+    config = TemplateConfig(
+        id="Config",
+        pattern_path=r"/path/{project}/{episode:ep\d+}/{shot:sh\d+}/scripts",
+        pattern_base=r"{project}_{episode}_{shot}_v{version}.nk",
+        default_expression=r"[\w_.-]+",
+        match_start=True,
+        match_end=True,
+        outputs=None,
+    )
+
+    token_mapping = {}
+
+    result = nomenclator.utilities.fetch_template_config(
+        path, [config], token_mapping
+    )
+    assert result is None
+    assert token_mapping == {}
+
+
+def test_fetch_template_config_match_flexible_start():
+    """Same scenario as previous test but with flexible start anchor."""
+    import nomenclator.utilities
+    from nomenclator.config import TemplateConfig
+
+    path = "/root/path/my_project/ep002/sh004/scripts"
+
+    config = TemplateConfig(
+        id="Config",
+        pattern_path=r"/path/{project}/{episode:ep\d+}/{shot:sh\d+}/scripts",
+        pattern_base=r"{project}_{episode}_{shot}_v{version}.nk",
+        default_expression=r"[\w_.-]+",
+        match_start=False,
+        match_end=True,
+        outputs=None,
+    )
+
+    token_mapping = {}
+
+    result = nomenclator.utilities.fetch_template_config(
+        path, [config], token_mapping
+    )
+    assert result == config
+    assert token_mapping == {
+        "project": "my_project",
+        "episode": "ep002",
+        "shot": "sh004",
+    }
